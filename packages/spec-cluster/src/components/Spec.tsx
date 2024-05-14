0266382c29ec4f050cc9f567c48844ff9fe0f145
Emmanuela Mensah <em530150@dal.ca>
2024-05-14 15:54:44 -0300

    added click event to spec component

--- packages/spec-cluster/src/components/Spec.tsx
+++ packages/spec-cluster/src/components/Spec.tsx
@@ -1,17 +1,31 @@
-import { useTexture } from "@react-three/drei"
+import { Html, useTexture } from "@react-three/drei"
 import type * as ThreeFiber from "@react-three/fiber"
+import * as React from "react"
 
 export type SpecProps = {
   position: ThreeFiber.MeshProps["position"]
   url: string
+  id: string
 }
 
 export default function Spec(props: SpecProps) {
+  const [isClicked, setIsClicked] = React.useState(false)
   const texture = useTexture(props.url)
   return (
-    <mesh position={props.position}>
+    // biome-ignore lint/a11y/useKeyWithClickEvents: <explanation>
+    <mesh
+      position={props.position}
+      onClick={() => {
+        setIsClicked(prevState => !prevState)
+      }}
+    >
       <planeGeometry args={[3, 3, 64, 64]} />
       <meshStandardMaterial map={texture} />
+      {isClicked && (
+        <Html>
+          <div>{props.id}</div>
+        </Html>
+      )}
     </mesh>
   )
 }
