1e4e8b9b257aa90e6f060cb5aff98b77288ec842
Emmanuela Mensah <em530150@dal.ca>
2024-05-14 15:54:08 -0300

    added a color type and click event to sphere component

--- packages/spec-cluster/src/components/Sphere.tsx
+++ packages/spec-cluster/src/components/Sphere.tsx
@@ -1,15 +1,31 @@
+import { Html } from "@react-three/drei"
 import type * as ThreeFiber from "@react-three/fiber"
+import * as React from "react"
 
 export type SphereProps = {
   position: ThreeFiber.MeshProps["position"]
   size: ThreeFiber.SphereGeometryProps["args"]
+  color: ThreeFiber.MeshStandardMaterialProps["color"]
+  id: string
 }
 
 export default function Sphere(props: SphereProps) {
+  const [isClicked, setIsClicked] = React.useState(false)
   return (
-    <mesh position={props.position}>
+    // biome-ignore lint/a11y/useKeyWithClickEvents: <explanation>
+    <mesh
+      position={props.position}
+      onClick={() => {
+        setIsClicked(prevState => !prevState)
+      }}
+    >
       <sphereGeometry args={props.size} />
-      <meshStandardMaterial color={"lightblue"} />
+      <meshStandardMaterial color={props.color} />
+      {isClicked && (
+        <Html>
+          <div>{props.id}</div>
+        </Html>
+      )}
     </mesh>
   )
 }
