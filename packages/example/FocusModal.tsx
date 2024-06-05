a1a20ef361d3ce5bfd16b80f1e00e3b5b3aeab3b
Emmanuela Mensah <em530150@dal.ca>
2024-06-05 08:59:57 -0300

    added styling for modal

--- packages/example/FocusModal.tsx
+++ packages/example/FocusModal.tsx
@@ -3,9 +3,46 @@ import { Focus } from "spec-cluster"
 
 export default function FocusModal() {
   const { focusedItem, hasFocus, unsetFocus } = Focus.useContext()
+  if (!focusedItem) {
+    return null
+  }
+  //TODO need to define mui props similar to maipl.
   return (
     <M.Modal open={hasFocus} onClose={unsetFocus}>
-      <pre>{JSON.stringify(focusedItem, null, 2)}</pre>
+      <M.Box
+        component={"div"}
+        sx={{
+          position: "absolute",
+          top: "50%",
+          left: "50%",
+          transform: "translate(-50%, -50%)",
+          width: 400,
+          bgcolor: M.colors.grey[50],
+          border: "1px solid #000",
+          borderRadius: "2px",
+          boxShadow: 24,
+          p: 4,
+          overflow: "hidden",
+        }}
+      >
+        <M.Typography variant="h6" component="h2" fontWeight={"bold"}>
+          Spectrogram Details
+        </M.Typography>
+        <img
+          alt={"spectrogram"}
+          src={`/spectrogram_plots/${focusedItem.filename}_spectrogram.png`}
+          style={{ maxWidth: "100%", maxHeight: "300px", margin: "16px 0" }}
+        />
+        <pre
+          style={{
+            backgroundColor: "#f4f4f4",
+            padding: "16px",
+            borderRadius: "4px",
+          }}
+        >
+          {JSON.stringify(focusedItem, null, 2)}
+        </pre>
+      </M.Box>
     </M.Modal>
   )
 }
