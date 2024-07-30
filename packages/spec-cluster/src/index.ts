674391d5bd786cb680e4e6a0818a90bc76f5d50b
Emmanuela Mensah <em530150@dal.ca>
2024-07-30 11:22:26 -0300

    removed customisation context

--- packages/spec-cluster/src/index.ts
+++ packages/spec-cluster/src/index.ts
@@ -26,6 +26,4 @@ export * as Focus from "./contexts/Focus"
 
 export * as ClickMode from "./contexts/ClickMode"
 
-export * as Customisation from "./contexts/Customisation"
-
 export * as UserData from "./contexts/UserData"
