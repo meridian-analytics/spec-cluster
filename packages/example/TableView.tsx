5ac5d75881c82ef3698c43538ac00e4af2658ec1
Emmanuela Mensah <em530150@dal.ca>
2024-08-22 10:23:43 -0300

    added color, flocation and label props to table view

--- packages/example/TableView.tsx
+++ packages/example/TableView.tsx
@@ -9,9 +9,10 @@ export default function TableView() {
   const selectedId = selectedIds[0]
   const spectrogram = spectrograms.get(selectedId)
 
-  if (spectrogram == null) {
-    throw Error(`TableView could not find spectrogram by id: ${selectedId}`)
-  }
+  //need to refactor this error message
+  // if (spectrogram == null) {
+  //   throw Error(`TableView could not find spectrogram by id: ${selectedId}`)
+  // }
   return (
     <div
       style={{
@@ -26,28 +27,43 @@ export default function TableView() {
         <M.Table stickyHeader>
           <M.TableHead>
             <M.TableRow>
+              {/* need to change entries to high level data */}
               <M.TableCell style={{ fontWeight: "bold" }}>Filename</M.TableCell>
               <M.TableCell style={{ fontWeight: "bold" }}>Dim1</M.TableCell>
               <M.TableCell style={{ fontWeight: "bold" }}>Dim2</M.TableCell>
               <M.TableCell style={{ fontWeight: "bold" }}>Dim3</M.TableCell>
+              <M.TableCell style={{ fontWeight: "bold" }}>Color</M.TableCell>
+              <M.TableCell style={{ fontWeight: "bold" }}>
+                Location(Made Up)
+              </M.TableCell>
+              <M.TableCell style={{ fontWeight: "bold" }}>Label</M.TableCell>
             </M.TableRow>
           </M.TableHead>
           <M.TableBody>
-            {selectedIds.map(id => {
-              const spectrogram = spectrograms.get(id)
-              return spectrogram ? (
-                <M.TableRow key={id}>
-                  <M.TableCell>{spectrogram.filename}</M.TableCell>
-                  <M.TableCell>{spectrogram.dim1}</M.TableCell>
-                  <M.TableCell>{spectrogram.dim2}</M.TableCell>
-                  <M.TableCell>{spectrogram.dim3}</M.TableCell>
-                </M.TableRow>
-              ) : (
-                <M.TableRow key={id}>
-                  <M.TableCell colSpan={4}>No data available</M.TableCell>
-                </M.TableRow>
-              )
-            })}
+            {selectedIds.length === 0 ? (
+              <M.TableRow>
+                <M.TableCell colSpan={7}>No spectrograms selected</M.TableCell>
+              </M.TableRow>
+            ) : (
+              selectedIds.map(id => {
+                const spectrogram = spectrograms.get(id)
+                return spectrogram ? (
+                  <M.TableRow key={id}>
+                    <M.TableCell>{spectrogram.filename}</M.TableCell>
+                    <M.TableCell>{spectrogram.dim1}</M.TableCell>
+                    <M.TableCell>{spectrogram.dim2}</M.TableCell>
+                    <M.TableCell>{spectrogram.dim3}</M.TableCell>
+                    <M.TableCell>{spectrogram.color}</M.TableCell>
+                    <M.TableCell>{spectrogram.flocation}</M.TableCell>
+                    <M.TableCell>{spectrogram.label}</M.TableCell>
+                  </M.TableRow>
+                ) : (
+                  <M.TableRow key={id}>
+                    <M.TableCell colSpan={7}>No data available</M.TableCell>
+                  </M.TableRow>
+                )
+              })
+            )}
           </M.TableBody>
         </M.Table>
       </M.TableContainer>
