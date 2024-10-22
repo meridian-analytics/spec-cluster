c9e9f7e5b04b829e56e1bb9f4f321c7a4de7c8d1
Emmanuela Mensah <em530150@dal.ca>
2024-10-22 00:13:45 -0300

    fixed shape sizing issue

--- packages/spec-cluster/src/components/Shape.tsx
+++ packages/spec-cluster/src/components/Shape.tsx
@@ -2,10 +2,14 @@ import { Html } from "@react-three/drei"
 import type * as ThreeFiber from "@react-three/fiber"
 import { BackSide } from "three"
 
+export type ShapeGeometryProps =
+  | { type: "Sphere"; args: ThreeFiber.SphereGeometryProps["args"] }
+  | { type: "Box"; args: ThreeFiber.BoxGeometryProps["args"] }
+  | { type: "Cone"; args: ThreeFiber.ConeGeometryProps["args"] }
+
 export type ShapeProps = {
   position: ThreeFiber.MeshProps["position"]
-  // size: ThreeFiber.SphereGeometryProps["args"]
-  size: ThreeFiber.BoxGeometryProps["args"]
+  size: ShapeGeometryProps
   color: ThreeFiber.MeshStandardMaterialProps["color"]
   id: string
   onClick?: ThreeFiber.MeshProps["onClick"]
@@ -14,7 +18,7 @@ export type ShapeProps = {
   isSelected?: boolean
   shape: string
 }
-//todo: need to rename this component to 'shape' or something else
+
 // adjsut sizing issue
 export default function Shape(props: ShapeProps) {
   return (
@@ -22,16 +26,16 @@ export default function Shape(props: ShapeProps) {
     <group position={props.position} onClick={props.onClick}>
       {props.isSelected && (
         <mesh scale={[1.3, 1.3, 1.3]}>
-          {props.shape === "Sphere" && <sphereGeometry />}
-          {props.shape === "Cube" && <boxGeometry />}
-          {props.shape === "Pyramid" && <coneGeometry args={[1, 1.5, 3]} />}
+          {props.shape === "Sphere" && <sphereGeometry args={props.size} />}
+          {props.shape === "Cube" && <boxGeometry args={props.size} />}
+          {props.shape === "Pyramid" && <coneGeometry args={props.size} />}
           <meshBasicMaterial color="black" side={BackSide} />
         </mesh>
       )}
       <mesh userData={{ id: props.id }}>
-        {props.shape === "Sphere" && <sphereGeometry />}
-        {props.shape === "Cube" && <boxGeometry />}
-        {props.shape === "Pyramid" && <coneGeometry args={[20, 1.5, 3]} />}
+        {props.shape === "Sphere" && <sphereGeometry args={props.size} />}
+        {props.shape === "Cube" && <boxGeometry args={props.size} />}
+        {props.shape === "Pyramid" && <coneGeometry args={props.size} />}
         <meshStandardMaterial color={props.color} />
       </mesh>
       {props.showID && (
@@ -42,15 +46,3 @@ export default function Shape(props: ShapeProps) {
     </group>
   )
 }
-
-// export type ShapeSize =
-//   | { type: "Sphere"; args: ThreeFiber.SphereGeometryProps["args"] } // [radius, widthSegments?, heightSegments?]
-//   | { type: "Box"; args: ThreeFiber.BoxGeometryProps["args"] } // [width, height, depth]
-//   | { type: "Cone"; args: ThreeFiber.ConeGeometryProps["args"] } // [radius, height, radialSegments]
-// size: ShapeSize // Use the unified size type
-
-// const sizeConfig = {
-//   Sphere: { type: "Sphere", args: [1, 32, 32] }, // radius, widthSegments, heightSegments
-//   Box: { type: "Box", args: [1, 1, 1] }, // width, height, depth
-//   Cone: { type: "Cone", args: [1, 2, 32] }, // radius, height, radialSegments
-// }
