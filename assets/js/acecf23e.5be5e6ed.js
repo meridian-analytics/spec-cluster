d94c7d92f74c29d768fba488f3419f7d35394e3e
macek <macek@dal.ca>
2024-11-01 10:40:43 -0400

    Deploy website - based on 0579a4f55dfae4e71e5ad141c6879f7b4ca6973d

---
+++ assets/js/acecf23e.5be5e6ed.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkwebsite=self.webpackChunkwebsite||[]).push([[1903],{1912:e=>{e.exports=JSON.parse('{"blogBasePath":"/spec-cluster/blog","blogTitle":"Blog","authorsListPath":"/spec-cluster/blog/authors"}')}}]);
---
+++ assets/js/c1b9e5a5.b887370c.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkwebsite=self.webpackChunkwebsite||[]).push([[5840],{9590:e=>{e.exports=JSON.parse('{"metadata":{"permalink":"/spec-cluster/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":4,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
