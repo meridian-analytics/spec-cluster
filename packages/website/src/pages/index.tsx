167b1ae13365cae684e3120ef5924f3bc80f6d03
Emmanuela Mensah <em530150@dal.ca>
2024-11-12 13:31:14 -0400

    updated home page

--- packages/website/src/pages/index.tsx
+++ packages/website/src/pages/index.tsx
@@ -1,13 +1,23 @@
-import clsx from "clsx"
-import Link from "@docusaurus/Link"
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext"
-import Layout from "@theme/Layout"
-import HomepageFeatures from "@site/src/components/HomepageFeatures"
 import Heading from "@theme/Heading"
-
+import Layout from "@theme/Layout"
+import clsx from "clsx"
+import type * as React from "react"
 import styles from "./index.module.css"
 
-function HomepageHeader() {
+export default function Home() {
+  const { siteConfig } = useDocusaurusContext()
+  return (
+    <Layout title={siteConfig.title} description={siteConfig.tagline}>
+      <Header />
+      <main>
+        <Features />
+      </main>
+    </Layout>
+  )
+}
+
+function Header() {
   const { siteConfig } = useDocusaurusContext()
   return (
     <header className={clsx("hero hero--primary", styles.heroBanner)}>
@@ -21,17 +31,77 @@ function HomepageHeader() {
   )
 }
 
-export default function Home(): JSX.Element {
-  const { siteConfig } = useDocusaurusContext()
+type FeatureProps = {
+  title: string
+  Svg: React.ComponentType<React.ComponentProps<"svg">>
+  description: JSX.Element
+}
+
+const features: FeatureProps[] = [
+  {
+    title: "Interactive Cluster Visualizations",
+    Svg: require("@site/static/img/undraw_instant_analysis_re_mid5.svg")
+      .default,
+    description: (
+      <>
+        Explore and interact with spectrogram data through dynamic clusters for
+        deeper audio insights.
+      </>
+    ),
+  },
+  {
+    title: "Customizable Visualization Tools",
+    Svg: require("@site/static/img/undraw_adjustments_re_gvct.svg").default,
+    description: (
+      <>
+        Tailor your visualizations by customizing spectrogram displays and
+        zooming in for more detailed analysis.
+      </>
+    ),
+  },
+  {
+    title: "Seamless React Integration",
+    Svg: require("@site/static/img/undraw_react_re_g3ui.svg").default,
+    description: (
+      <>
+        Leverage the power of React to integrate SpecCluster into your
+        applications effortlessly.
+      </>
+    ),
+  },
+]
+
+function Feature(props: FeatureProps) {
   return (
-    <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />"
+    <div className={clsx("col col--4")}>
+      <div className="text--center">
+        <props.Svg width={200} height={200} role="img" />
+      </div>
+      <div className="text--center padding-horiz--md">
+        <Heading as="h3">{props.title}</Heading>
+        <p>{props.description}</p>
+      </div>
+    </div>
+  )
+}
+
+function Features() {
+  return (
+    <section
+      style={{
+        display: "flex",
+        alignItems: "center",
+        padding: "2rem 0",
+        width: "100%",
+      }}
     >
-      <HomepageHeader />
-      <main>
-        <HomepageFeatures />
-      </main>
-    </Layout>
+      <div className="container">
+        <div className="row">
+          {features.map(props => (
+            <Feature key={props.title} {...props} />
+          ))}
+        </div>
+      </div>
+    </section>
   )
 }
